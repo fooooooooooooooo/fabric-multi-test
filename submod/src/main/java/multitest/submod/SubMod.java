230afe7eee12a1e96b72788b089ba4a09f70c03d
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2022-10-31 02:49:56 -0700

    sub mod

--- submod/src/main/java/multitest/submod/SubMod.java
+++ submod/src/main/java/multitest/submod/SubMod.java
@@ -1,17 +1,17 @@
-package multitest;
+package multitest.submod;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import multitest.api.MultitestApi;
 
-public class Multitest implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("multitest");
+public class SubMod implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("multitest-submod");
 
     @Override
     public void onInitialize() {
         var message = new MultitestApi().hello();
 
-        LOGGER.info(message + " from main mod");
+        LOGGER.info(message + " from sub mod");
     }
 }
